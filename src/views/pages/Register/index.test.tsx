3a41ef3be1c23ec74c327cca1e36d0b17ae55850
Anh Tuan Hoang <anhht@scuti.asia>
2023-01-11 11:29:58 +0700

    update

--- src/views/pages/Register/index.test.tsx
+++ src/views/pages/Register/index.test.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import Register from ".";
+import { act } from "react-dom/test-utils";
 
 test("Register shows default value", async () => {
   render(<Register />);
-  const data = screen.getByText(/about/i);
-  console.log(data);
-  // expect(screen.getAllByRole("button", { hidden: true }));
+  await act(async () => {
+    fireEvent.change(screen.getByLabelText("username"), {
+      target: { value: "0966666666" },
+    });
+    fireEvent.change(screen.getByLabelText("password"), {
+      target: { value: "1232113" },
+    });
+    fireEvent.click(screen.getByRole("button", { name: "Login" }));
+  });
+  expect(screen.getByLabelText("username")).toHaveValue("0966666666");
+  expect(screen.getByLabelText("password")).toHaveValue("1232113");
+  expect(screen.getByTestId("username")).toBeInTheDocument();
+  expect(screen.getByTestId("password")).toBeInTheDocument();
 });
+
+// it("renders with or without a name", () => {
+//   act(async () => {
+//     render(<Register />);
+//     const button = screen.getByRole("button", { name: "Change Button" });
+//     fireEvent.click(button);
+//   });
+//   expect(screen.getByText("Initial State").textContent).toBe(
+//     "Initial State Changed"
+//   );
+// });
--- src/views/pages/Register/index.tsx
+++ src/views/pages/Register/index.tsx
@@ -1,78 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Checkbox, Form, Input, Space } from "antd";
 
 export default function Register() {
+  const [data, setData] = useState<{ username?: string; password?: string }>();
   const onFinish = (values: any) => {
     console.log("Success:", values);
-    alert(JSON.stringify(values));
+    setData(values);
   };
 
   const onFinishFailed = (errorInfo: any) => {
     console.log("Failed:", errorInfo);
   };
 
+  const [state, setState] = useState("Initial State");
+
+  const changeState = () => {
+    setState("Initial State Changed");
+  };
+
   return (
-    <div>
-      <label htmlFor="username-input">Username</label>
-      <input id="username-input" />
-      <label id="username-label">Username</label>
-      <input aria-labelledby="username-label" />
-      <label>
-        Username <input />
-      </label>
-      <label>
-        <span>Username</span>
-        <input />
-      </label>
-      <input aria-labelledby="Username" placeholder="Test" />
-    </div>
-    // <Space
-    //   align="center"
-    //   direction="horizontal"
-    //   style={{ flexDirection: "column" }}
-    // >
-    //   <h1>Login Text</h1>
-    //   <Form
-    //     name="basic"
-    //     labelCol={{ span: 8 }}
-    //     wrapperCol={{ span: 16 }}
-    //     initialValues={{ remember: true }}
-    //     onFinish={onFinish}
-    //     onFinishFailed={onFinishFailed}
-    //     autoComplete="off"
-    //   >
-    //     <Form.Item
-    //       label="Username"
-    //       name="username"
-    //       rules={[{ required: true, message: "Please input your username!" }]}
-    //     >
-    //       <Input />
-    //     </Form.Item>
-    //     <Form.Item
-    //       label="Password"
-    //       name="password"
-    //       rules={[{ required: true, message: "Please input your password!" }]}
-    //     >
-    //       <Input.Password />
-    //     </Form.Item>
-    //     <div>
-    //       <Form.Item
-    //         name="remember"
-    //         valuePropName="checked"
-    //         wrapperCol={{ offset: 8, span: 16 }}
-    //         noStyle
-    //       >
-    //         <Checkbox>Remember me</Checkbox>
-    //       </Form.Item>
-    //     </div>
-    //     <div style={{ marginTop: "1rem" }}>
-    //       <Form.Item wrapperCol={{ offset: 8, span: 16 }} noStyle>
-    //         <Button type="primary" htmlType="submit">
-    //           Login
-    //         </Button>
-    //       </Form.Item>
-    //     </div>
-    //   </Form>
-    // </Space>
+    <Space
+      align="center"
+      direction="horizontal"
+      style={{ flexDirection: "column" }}
+    >
+      <h1>Login Text</h1>
+      {data?.username && (
+        <span data-testid="username">{`UserName : ${data?.username}`}</span>
+      )}
+      {data?.password && (
+        <span data-testid="password">{`password : ${data?.password}`}</span>
+      )}
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input aria-label="username" />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password aria-label="password" />
+        </Form.Item>
+        <div>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+            noStyle
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+        </div>
+        <div style={{ marginTop: "1rem" }}>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }} noStyle>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </div>
+      </Form>
+    </Space>
   );
 }
